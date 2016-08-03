5a56b01169ca82cc4b7904226bb3e07238f3a6ce
luyb <luyb@020121718-NB.servyou.local>
2016-08-03 15:45:26 +0800

    单例

--- src/main/java/com/study/jvm/vola/Singleton.java
+++ src/main/java/com/study/jvm/vola/Singleton.java
@@ -1,10 +1,20 @@
 package com.study.jvm.vola;
 
 public class Singleton {
-	private volatile static Singleton instance;
+	//private volatile static Singleton instance;
+	//½ûÖ¹ÖØÅÅÐò
+	private  volatile static Singleton instance;
 	
 	private Singleton(){};
 	
+	
+	public static Singleton getInstance_wrong() {
+		if (instance == null)
+			instance = new Singleton();
+		return instance;
+	}
+	
+	//DCLË«ÖØ¼ì²éËø  bug
 	public static Singleton getInstance() {
 		if (instance == null) {
 			synchronized(Singleton.class) {
@@ -14,8 +24,10 @@ public class Singleton {
 		}
 		return instance;
 	}
+	
 	public static void main(String[] args) {
 		Singleton.getInstance();
 	}
+	
 
 }
