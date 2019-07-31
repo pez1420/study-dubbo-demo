59d628efb3d4510806035c0963c33124b98e8880
luyb <luyb@servyou.com.cn>
2019-07-31 16:12:27 +0800

    ceshi

--- src/main/java/com/study/dubbo/concurrent/callable/TestCallable.java
+++ src/main/java/com/study/dubbo/concurrent/callable/TestCallable.java
@@ -4,36 +4,36 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 class CallThread implements Callable<Integer> {
-	
-	public Integer call() {
-		int i = 0;
-		for (; i < 60; i++) {
-			System.out.println(Thread.currentThread().getName() + "循环变量i的值是 : " + i);
-		}
-		return i;
-	}
+
+    public Integer call() {
+        int i = 0;
+        for (; i < 60; i++) {
+            System.out.println(Thread.currentThread().getName() + "循环变量i的值是 : " + i);
+        }
+        return i;
+    }
 }
 
 public class TestCallable {
 
-	public static void main(String[] args) {
-		// 创建Callable对象
-		CallThread call = new CallThread();
-		// 使用FutrueTask来包装Callable对象
-		FutureTask<Integer> task = new FutureTask<Integer>(call);
-		for (int i = 0; i < 60; i++) {
-			System.out.println(Thread.currentThread().getName() + "-------" + i);
-			if (i == 10) {
-				// 实际上还是以Callable对象创建并启动线程
-				new Thread(task, "有返回值的线程").start(); 
-			}
-		}
-		
-		try {
-			System.out.println("子线程的返回值 ：" + task.get());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        // 创建Callable对象
+        CallThread call = new CallThread();
+        // 使用FutrueTask来包装Callable对象
+        FutureTask<Integer> task = new FutureTask<Integer>(call);
+        for (int i = 0; i < 60; i++) {
+            System.out.println(Thread.currentThread().getName() + "-------" + i);
+            if (i == 10) {
+                // 实际上还是以Callable对象创建并启动线程
+                new Thread(task, "有返回值的线程").start();
+            }
+        }
+
+        try {
+            System.out.println("子线程的返回值 ：" + task.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/main/java/com/study/dubbo/concurrent/callable/TestCompletableFuture.java
@@ -0,0 +1,47 @@
+package com.study.dubbo.concurrent.callable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author pez1420@gmail.com
+ * @version $Id: TestCompletableFuture.java v 0.1 2019/5/7 3:51 PM pez1420 Exp $$
+ */
+public class TestCompletableFuture {
+
+    public static CompletableFuture<Integer> compute() {
+        final CompletableFuture<Integer> future = new CompletableFuture<>();
+        return future;
+    }
+
+    public static void main(String[] args) throws Exception {
+        final CompletableFuture<Integer> f = compute();
+        class Client extends Thread {
+            CompletableFuture<Integer> f;
+
+            Client(String threadName, CompletableFuture<Integer> f) {
+                super(threadName);
+                this.f = f;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    System.out.println(this.getName() + ": " + f.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        new Client("Client1", f).start();
+        new Client("Client2", f).start();
+        System.out.println("waiting");
+        f.complete(100);
+        //f.completeExceptionally(new Exception());
+        System.in.read();
+    }
+
+
+}
